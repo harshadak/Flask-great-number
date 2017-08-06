9067f9b9010321d6477cad3438bd6de37ebd5d2c
Harshada Kurahatti <harshada.kurahatti@gmail.com>
2017-08-06 16:23:45 -0700

    Clean up

--- index.py
+++ index.py
@@ -8,10 +8,8 @@ app.secret_key = "Secret"
 @app.route("/")
 def index():
     message = ""
-    print session
     if "guess_number" not in session:
         session["random_number"] = random.randrange(0, 101)
-        print session
     elif session["guess_number"] < session["random_number"]:
         message = "low"
     elif session["guess_number"] > session["random_number"]:
